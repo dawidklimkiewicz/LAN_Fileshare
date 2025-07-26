3d6ab946c35fbac13fb01e81915ef1afffc8e9b1
Dawid Klimkiewicz <82545462+dawidklimkiewicz@users.noreply.github.com>
2025-07-26 22:31:20 +0200

    drag & drop label

---
+++ LAN_Fileshare/Converters/IntToBoolNegatedConverter.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAN_Fileshare.Converters
+{
+    class IntToBoolNegatedConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is int intValue)
+            {
+                return intValue > 0 ? false : true;
+            }
+            return true;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
